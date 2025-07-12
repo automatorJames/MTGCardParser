6c3d184d8eb659cb960344bcdcf89458a02b6824
James <automator.james@gmail.com>
2025-07-12 11:57:39 -0500

    Fixed nested property tables. Refined highlighting.

---
+++ MTGPlexer/TokenUnits/test.cs
@@ -0,0 +1,24 @@
+﻿namespace MTGPlexer.TokenUnits;
+
+public class Test : TokenUnit
+{
+    public Test() : base("graveyard", nameof(TestChild)) { }
+    public TestChild TestChild { get; set; }
+
+}
+
+public class TestChild : TokenUnit
+{
+    public TestChild() : base("from the", nameof(TestChildChild)) { }
+
+    public TestChildChild TestChildChild { get; set; }
+
+}
+
+public class TestChildChild : TokenUnit
+{
+    public TestChildChild() : base("battlefield") { }
+
+}
+
+
