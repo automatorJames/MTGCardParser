a0c6ae67c170af879b6dde7f2917c32e3db1423d
James <automator.james@gmail.com>
2026-01-17 13:28:47 -0600

    Restoring the Ankh of Mishra

--- MTGPlexer/TokenUnits/ThatCardsController.cs
+++ MTGPlexer/TokenUnits/ThatCardsController.cs
@@ -2,9 +2,7 @@
 
 public class ThatCardsController : TokenUnit
 {
-    protected override Snippet[] Snippets => ["that", Prop(CardOrCreatureType), "'s controller", Prop(SacrificeIt)];
+    protected override Snippet[] Snippets => ["that", Prop(CardOrCreatureType), "'s controller"];
 
     public OneOf<CardType, CreatureType> CardOrCreatureType { get; set; }
-
-    public OptionalOf<SacrificeIt> SacrificeIt { get; set; }
 }
