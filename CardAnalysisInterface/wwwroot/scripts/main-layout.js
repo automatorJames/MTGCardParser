4d0c71cfc4f7e8f716253a6d9eba158d99253f77
James <automator.james@gmail.com>
2026-01-18 09:12:14 -0600

    Commenting out keystroke listeners from the main b/c we're not really using them and they're interfering with other JS code

--- CardAnalysisInterface/wwwroot/scripts/main-layout.js
+++ CardAnalysisInterface/wwwroot/scripts/main-layout.js
@@ -1,25 +1,25 @@
 ﻿let dotNetRef;
 
-function handleLayoutKeydown(e) {
-    //// Exit unless Shift + Left/Right Arrow
-    //if (!e.shiftKey || (e.key !== 'ArrowRight' && e.key !== 'ArrowLeft')) {
-    //    return;
-    //}
-    //
-    //e.preventDefault();
-    //
-    //if (e.key === 'ArrowRight') {
-    //    dotNetRef.invokeMethodAsync('CyclePage', 'forward');
-    //} else {
-    //    dotNetRef.invokeMethodAsync('CyclePage', 'backward');
-    //}
-}
-
-function initializeLayoutListener(dotNetObjectReference) {
-    dotNetRef = dotNetObjectReference;
-    document.addEventListener('keydown', handleLayoutKeydown);
-}
-
-function disposeLayoutListener() {
-    document.removeEventListener('keydown', handleLayoutKeydown);
-}
+//function handleLayoutKeydown(e) {
+//    //// Exit unless Shift + Left/Right Arrow
+//    //if (!e.shiftKey || (e.key !== 'ArrowRight' && e.key !== 'ArrowLeft')) {
+//    //    return;
+//    //}
+//    //
+//    //e.preventDefault();
+//    //
+//    //if (e.key === 'ArrowRight') {
+//    //    dotNetRef.invokeMethodAsync('CyclePage', 'forward');
+//    //} else {
+//    //    dotNetRef.invokeMethodAsync('CyclePage', 'backward');
+//    //}
+//}
+//
+//function initializeLayoutListener(dotNetObjectReference) {
+//    dotNetRef = dotNetObjectReference;
+//    document.addEventListener('keydown', handleLayoutKeydown);
+//}
+//
+//function disposeLayoutListener() {
+//    document.removeEventListener('keydown', handleLayoutKeydown);
+//}
